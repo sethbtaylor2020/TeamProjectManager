9df7c39483de35b84889f47d0ee00dfe5b11f3bd
kadenboam <boa24015@byui.edu>
2026-01-30 15:00:05 -0700

    changed_from_react_strict_to_react_router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     { id: 2, name: "John" },
     { id: 3, name: "Alice" },
     { id: 4, name: "Bob" }
-  ];
+  ]
 
   //This will be replaced by a database pull as well
   const assignments = [
@@ -16,6 +17,7 @@ function App() {
     { id: 3, userId: 3, text: "Create UI mockups" },
     { id: 4, userId: 4, text: "Set up database schema" }
   ]
+<<<<<<< HEAD
 
   function setTheme(theme) {
     const root = document.documentElement;
@@ -37,27 +39,31 @@ function App() {
       root.style.setProperty("--text-color", "#3a1f04");
     }
   }
+=======
+>>>>>>> 16c7a70 (changed_from_react_strict_to_react_router)
 
   return (
     <>
+      <Link to="/page2">Go to Page 2</Link>
+
       <div>
         <h1>Team Project Helper</h1>
       </div>
+
       <div className="Columns">
         <div className="Column">
           <h2>Team</h2>
         </div>
+
         {users.map(user => (
-          /* Users flow through here */
           <div key={user.id} className="Column">
             <h2>{user.name}</h2>
-            {/* Assignments flow through this */}
+
             {assignments
               .filter(a => a.userId === user.id)
               .map(a => (
                 <p key={a.id}>{a.text}</p>
-              ))
-            }
+              ))}
           </div>
         ))}
       </div>
