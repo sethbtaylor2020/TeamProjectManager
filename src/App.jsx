7fe0eb919b865e9c84cb3b0be8b27e3915a828cc
kadenboam <boa24015@byui.edu>
2026-02-18 14:27:03 -0700

    Added_animation_for_expanding_tasks_and_themes

--- src/App.jsx
+++ src/App.jsx
@@ -34,11 +34,7 @@ function App() {
     { id: 2, name: "John" },
     { id: 3, name: "Alice" },
     { id: 4, name: "Bob" }
-  ]
-
-// TEST!!!! IF BROKEN COMMENT OUT
-// console.log(supabase)
-// END TEST AREA
+  ];
 
   //This will be replaced by a database pull as well
   const assignments = [
@@ -47,7 +43,7 @@ function App() {
     { id: 3, userId: 3, text: "Create UI mockups" },
     { id: 4, userId: 4, text: "Set up database schema" }
   ]
-
+  //Selection of themes
   function setTheme(theme) {
     const root = document.documentElement;
     if (theme === "dark") {
@@ -71,33 +67,31 @@ function App() {
 
   return (
     <>
-      <Link to="/page2">Go to Page 2</Link>
-
       <div>
         <h1>Team Project Helper</h1>
       </div>
-
       <div className="Columns">
         <div className="Column">
           <h2>Team</h2>
         </div>
-
         {users.map(user => (
+          /* Users flow through here */
           <div key={user.id} className="Column">
             <h2>{user.name}</h2>
-
+            {/* Assignments flow through this */}
             {assignments
               .filter(a => a.userId === user.id)
               .map(a => (
                 <p key={a.id}>{a.text}</p>
-              ))}
+              ))
+            }
           </div>
         ))}
       </div>
       <button onClick={() => setShowThemeMenu(!showThemeMenu)}>Themes</button>
       <button onClick={() => setShowAssignmentMenu(!showAssignmentMenu)}>Add Tasks</button>
-      <div>
-        <form class="box" onSubmit={handleSubmit} className={showAssignmentMenu ? "assignment-visible" : "assingment-hidden"}>
+      <div className={`toggle ${showAssignmentMenu ? "assignment-visible" : "assignment-hidden"}`}>
+        <form onSubmit={handleSubmit}>
           <p>User Name</p>
           <input name="name" type="text" value={form.name} onChange={handleChange} placeholder="User name" />
           <p>Password</p>
@@ -108,7 +102,7 @@ function App() {
           <button type="submit">Submit</button>
         </form>
       </div>
-      <div id="theme" className={showThemeMenu ? "theme-visible" : "theme-hidden"}>
+      <div className={`toggle ${showThemeMenu ? "theme-visible" : "theme-hidden"}`}>
         <button onClick={() => setTheme("dark")}>Dark</button>
         <button onClick={() => setTheme("ocean")}>Ocean</button>
         <button onClick={() => setTheme("sunset")}>Sunset</button>
